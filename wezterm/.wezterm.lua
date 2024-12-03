2c80c8ab7689cfc9da8d179d2c93ea4dafacf755
David Dai <davidwh.dai@gmail.com>
2024-12-03 13:35:17 -0500

    add aerospace and wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,17 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.font_size = 24
+-- For example, changing the color scheme:
+config.color_scheme = "duskfox"
+config.window_background_opacity = 0.85
+config.window_frame = {
+	font_size = 20,
+}
+-- and finally, return the configuration to wezterm
+return config
